68750608ce4ff34c45dba63c3e5433025e2e42f5
MoSaber <msaberms72016@gmail.com>
2021-02-28 00:01:57 +0200

    Refactored chat sending code

--- ChatApp/ChatRoomViewController.swift
+++ ChatApp/ChatRoomViewController.swift
@@ -19,34 +19,62 @@ class ChatRoomViewController: UIViewController {
         
     }
     
-   
-
-    @IBAction func sendButtonTapped(_ sender: Any) {
+    func getUserNameByID(userId: String, compeletion: @escaping (_ userName: String?)->Void){
+        let refrence = Database.database().reference()
+        let user = refrence.child("users").child(userId)
         
-        guard let message = messageTextField.text, message.isEmpty == false, let userId = Auth.auth().currentUser?.uid else {
+        user.child("username").observeSingleEvent(of: .value) { (snapshot) in
+            if let userName = snapshot.value as? String{
+                compeletion(userName)
+            }else{
+                compeletion(nil)
+            }
+            
+            
+        }
+        
+    }
+    
+    func sendMessage(message: String, compeletion: @escaping (_ isSuccesfull: Bool)-> ()){
+        guard let userId = Auth.auth().currentUser?.uid else {
             return
         }
         
         let refrence = Database.database().reference()
         let user = refrence.child("users").child(userId)
         
-        user.child("username").observeSingleEvent(of: .value) { (snapshot) in
-            if let userName = snapshot.value as? String, let roomID = self.room?.roomID{
+        getUserNameByID(userId: userId) { (userName) in
+            if let userName = userName, let roomID = self.room?.roomID{
                 let dataDict : [String: Any] = ["username": userName, "text": message]
                 
                 let room = refrence.child("Room").child(roomID)
                 
                 room.child("messages").childByAutoId().setValue(dataDict) { (error, _) in
                     if error == nil {
-                        
+                        compeletion(true)
                         self.messageTextField.text = ""
                         print("message uploaded succesfully")
                     }
+                    compeletion(false)
                 }
                 
             }
         }
+    }
+    
+    
+    @IBAction func sendButtonTapped(_ sender: Any) {
         
+        guard let message = messageTextField.text, message.isEmpty == false else {
+            return
+        }
+        
+        sendMessage(message: message){
+            (isSuccesfull) in
+            if isSuccesfull{
+                print("yay")
+            }
+        }
         
     }
 }
